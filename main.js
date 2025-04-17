6b3a1164430caf8fd70bb84474951f7336821f5f
bcg <minaday41@gmail.com>
2025-04-17 19:38:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text",{
+	strings: ["Bandaranayake College" , "Technology Society" , "Official Website"],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop:true
+});
+
